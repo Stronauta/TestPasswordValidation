7a20a7bd34bbba058fe331f52d3a27ec1db3a8cf
Stronauta <samircastillop@gmail.com>
2023-11-07 17:41:44 -0500

    Se crearon las pruebas

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -2,7 +2,44 @@ namespace TestingPassord
 {
 	public class UnitTest1
 	{
-		
+		[Fact]
+		public void IsPasswordSecure_returns_false_if_password_has_less_than_8_characters()
+		{
+			//Arrange
+			var registerViewModel = new RegisterViewModel();
+
+			//Act
+			bool result = registerViewModel.IsPasswordSecure("1234567");
+
+			//Assert
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void IsPasswordSecure_returns_false_if_password_does_not_contains_uppercase_character()
+		{
+			//Arrange
+			var registerViewModel = new RegisterViewModel();
+
+			//Act
+			bool result = registerViewModel.IsPasswordSecure("12345678a");
+
+			//Assert
+			Assert.False(result);
+		}
+
+		[Fact]
+		public void IsPasswordSecure_returns_false_if_password_does_not_contains_symbol()
+		{
+			//Arrange
+			var registerViewModel = new RegisterViewModel();
+
+			//Act
+			bool result = registerViewModel.IsPasswordSecure("12345678");
+
+			//Assert
+			Assert.False(result);
+		}
 	}
 	internal class RegisterViewModel
 	{
